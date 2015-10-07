39901088d775dc190bc5dcb7512e82a1ec4bf17d
Matt Gitzendanner <magitz@ufl.edu>
2015-10-07 12:43:41 -0400

    Adding version that uses BioPython

---
+++ DNAcalc2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import argparse
+from Bio import SeqIO
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", help="Input file")
+
+args = parser.parse_args()
+
+InFile = args.i
+
+try:
+  IN=open(InFile, 'r')
+except IOError:
+  print ("Can't open file.")
+
+#for Line in IN:
+#  Line=Line.strip('\n')
+for Record in SeqIO.parse(IN, "fasta") :
+  DNAseq=Record.seq
+  SeqLength=len(DNAseq)
+  
+  GC_count=0
+  
+  for Base in ('A','G','T','C', 'N'):
+    NumBase=DNAseq.count(Base)
+    
+    #print("Percent %s: %.2f" %(Base,NumBase/SeqLength*100))
+    
+    if Base == "G" or Base == "C":
+           GC_count+=NumBase
+            
+  #print ("Sequence is %d bp long" %(SeqLength))
+  print ("GC content of %s is %.2f" %(Record.id, GC_count/SeqLength*100))
